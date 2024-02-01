0657371b2d1ea1716b90be1e967ea7ab4a554812
nagaranubhav <anubhav200a1@gmail.com>
2024-02-01 18:43:34 +0530

    added the datatable component

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,8 +1,15 @@
 import "./list.scss"
-
+import Sidebar from "../../components/sidebar/Sidebar"
+import Navbar from"../../components/navbar/Navbar"
+import Datatable from "../../components/datatable/Datatable"
 const List = () => {
   return (
-    <div>
+    <div className="list">
+      <Sidebar/>
+      <div className="listcontainer">
+        <Navbar/>
+        <Datatable/>
+      </div>
       list
     </div>
   )
